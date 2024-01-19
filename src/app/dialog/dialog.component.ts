86c074aaef8b85b45b509bc5e07f31c4f1c8a841
Souvik Biswas <souvik.biswas1998@gmail.com>
2024-01-20 02:31:42 +0530

    fix some known issue

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -31,18 +31,21 @@ export class DialogComponent {
   allStatus: string[] = ['To Do', 'In Progress', 'Complete'];
   priorities: string[] = ['High', 'Medium', 'Low'];
   id!: string
+  status: string = '';
 
   constructor(private dataService: DataService, public router: Router,
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public dialog_data: any
   ) {
+    if (dialog_data?.id) this.id = dialog_data.id;
     if (dialog_data?.data) this.initialForm(dialog_data?.data)
     else this.initialForm(data.find((task: any) => task.id == +dialog_data.id))
   }
 
   initialForm(data?: any) {
+    this.status = data?.status || 'To Do'
     this.formGroup = new FormGroup({
-      status: new FormControl(data?.status || 'To Do', Validators.required),
+      status: new FormControl(this.status, Validators.required),
       priority: new FormControl(data?.priority || 'Medium', Validators.required),
       dueDate: new FormControl(data?.dueDate || '', Validators.required),
       taskName: new FormControl(data?.taskName || '', Validators.required),
@@ -51,12 +54,15 @@ export class DialogComponent {
   }
 
   saveFormData() {
+    console.log(this.dialog_data);
+
     if (this.formGroup.invalid) return this.dataService.showSnackbar('all the form fields are reuired');
     let data2 = this.formGroup.value;
     data2.id = this.id || +new Date()
     if (this.id) {
       let index = data.findIndex((task: any) => task.id == +this.id)
-      data[index] = data2
+      data[index] = data2;
+      this.dataService.createtask(undefined)
     }
     else this.dataService.createtask(data2)
     this.router.navigate(['dashboard']);
@@ -66,6 +72,25 @@ export class DialogComponent {
   colapseOrExpand(colapse: boolean = true) {
     this.dialogRef.close();
     if (colapse) this.router.navigate([".."], { state: { data: this.formGroup.value } });
-    else this.router.navigate(['/task/' + (this.dialog_data?.id || 'new')], { state: { data: this.formGroup.value } })
+    else this.router.navigate([this.dialog_data?.id ? ('/task/' + this.dialog_data?.id) : '/create-task'], { state: { data: this.formGroup.value } })
+  }
+
+  setDisable(arg0: string): boolean {
+    let status = false;
+    switch (this.status) {
+      case 'To Do':
+        status = !(arg0 !== 'Complete')
+        break;
+      case 'In Progress':
+        status = false
+        break;
+      case 'Complete':
+        status = !(arg0 !== 'In Progress')
+        break;
+      default:
+        status = false
+        break;
+    }
+    return status
   }
 }
